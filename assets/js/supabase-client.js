2bdb4200bb7641487ea7526082b32f21b66cd335
AmicaleUIS <amicaleuis.m2000@gmail.com>
2026-03-12 09:56:43 +0100

    Add files via upload

--- assets/js/supabase-client.js
+++ assets/js/supabase-client.js
@@ -1,7 +1,6 @@
 const SUPABASE_URL = "https://xqtxrolktiqnqtmetmtc.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_oWEmzLtO8VFVD6Feh6nDhw_kZ_pIPKN";
 
-// On utilise le SDK global chargé par le script CDN
 window.sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     persistSession: true,
